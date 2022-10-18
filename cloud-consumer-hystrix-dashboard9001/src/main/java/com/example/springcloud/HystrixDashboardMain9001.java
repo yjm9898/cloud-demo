f9e048a1f65cbe1593c75380a012e49cbb2793d7
yin.jiemin <yjm9898@outlook.com>
2022-10-18 10:57:18 +0800

    添加hystrix  dashboard 熔断监控版

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/example/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,22 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by jiemin on 2022/10/18 10:22
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardMain9001 {
+
+    /**
+     * 主启动类
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+
+}

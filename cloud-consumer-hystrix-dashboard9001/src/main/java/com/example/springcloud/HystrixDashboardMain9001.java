89aab11a192186aa88aedeea538e8dadc97eeb24
yin.jiemin <yjm9898@outlook.com>
2022-10-20 18:36:08 +0800

    添加hystrix 监控仪表板， hystrix.stream 仪表板

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/example/springcloud/HystrixDashboardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/example/springcloud/HystrixDashboardMain9001.java
@@ -2,6 +2,7 @@ package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @EnableHystrixDashboard
 @SpringBootApplication
+@EnableEurekaClient
 public class HystrixDashboardMain9001 {
 
     /**

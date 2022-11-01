fd7284a99f3fe088f0c320c42f811d881a4b7150
yin.jiemin <yjm9898@outlook.com>
2022-11-01 13:17:30 +0800

    sentinel 模块新建

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/example/springcloud/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by jiemin on 2022/11/1 10:39
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+
+}
